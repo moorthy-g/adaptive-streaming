641eff18a859b3067f3dfb7fe0f248e7b2952251
Moorthy <moorthy.g@krds.fr>
2017-08-04 20:17:48 +0530

    Switch videojs player between HLS & Shaka DASH

--- src/js/videojsPlayer.js
+++ src/js/videojsPlayer.js
@@ -1,16 +1,37 @@
 import 'video.js/dist/video-js.css';
-
 window.videojs = require('video.js').default;
-require('videojs-contrib-hls/dist/videojs-contrib-hls');
 
-var instance = new Object();
+let instance = new Object(), baseURL = './video/';
 
 instance.init = function() {
-    this.player = window.videojs('player', {
+    let select = document.getElementById('techSelect'),
+        form = document.getElementById('techForm');
+    select.addEventListener('change', () => {
+        form.submit();
+    });
+    this.setPlayback();
+}
+
+instance.setPlayback = function() {
+    let select = document.getElementById('techSelect'),
+        value = location.search.replace('?tech=', '') || 'hls';
+
+    select.value = value;
+
+    if(value === 'shaka') {
+        this.shakaDashPlayback();
+    } else {
+        this.hlsPlayback();
+    }
+}
+
+instance.hlsPlayback = function() {
+    require('videojs-contrib-hls/dist/videojs-contrib-hls');
+
+    this.player = videojs('player', {
         controls: true,
         sources: [
-            { type:'application/x-mpegURL', src: './video/574825.m3u8' },
-            { type: 'video/mp4', src: './video/friendship.mp4' }
+            { type:'application/x-mpegURL', src: baseURL + '574825.m3u8' }
         ]
     })
     this.player.ready(() => {
@@ -18,4 +39,19 @@ instance.init = function() {
     })
 }
 
-export default instance
+instance.shakaDashPlayback = function() {
+    require('videojs-shaka-player');
+    require('videojs-shaka-player/dist/player-skin');
+
+    this.player = videojs('player', {
+        controls: true,
+        sources: [
+            { type:'application/dash+xml', src: baseURL + '574825.mpd' }
+        ]
+    })
+    this.player.ready(() => {
+        console.log('Videojs Player: Ready!')
+    })
+}
+
+export default instance;
